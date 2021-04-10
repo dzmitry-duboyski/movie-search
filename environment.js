1ebef5774315d7ddb9794acaf03bfb3e140e0740
Dzmitry Duboyski <dzmitry.duboyski@gmail.com>
2021-04-11 00:33:31 +0300

    refactor: add netlify environment

--- environment.js
+++ environment.js
@@ -1,4 +1,4 @@
 const OMD_API_KEY = 'd44d4c61';
-// const OMD_API_KEY = '5b0655a5';
+// // const OMD_API_KEY = '5b0655a5';
 
 export { OMD_API_KEY };
---
+++ readme.md
@@ -0,0 +1,1 @@
+## Deploy: https://dzmitry-duboyski-movie-search.netlify.app
